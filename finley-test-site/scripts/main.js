bb1c6a10c8026add17a0a5f39a764b2cc942737c
laurangarrison <lgarrison@berklee.edu>
2017-06-16 14:44:54 -0700

    Add files via upload

---
+++ finley-test-site/scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/finley-smile.jpg') {
+		myImage.setAttribute ('src','images/finleysmile2.jpg');
+	}else{
+		myImage.setAttribute ('src','images/finley-smile.jpg');
+	}
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Finley is cool, ' + myName;
+}
+if(!localStorage.getItem('name')){
+	setUserName();
+}else{
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Finley is cool, ' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
